7914caeb905bff7a6f17d036cf768f5e61f87024
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2023-11-29 10:10:55 +0530

    Here I find the total salary of each department.

---
+++ src/Assignment-4.sql
@@ -0,0 +1,42 @@
+-- Step 1: Create table and insert data
+CREATE TABLE dept_tbl (
+    id_deptname VARCHAR(50),
+    emp_name VARCHAR(50),
+    salary INT
+);
+
+-- Insert data
+INSERT INTO dept_tbl (id_deptname, emp_name, salary) VALUES
+('1111-MATH', 'RAHUL', 10000),
+('1111-MATH', 'RAKESH', 20000),
+('2222-SCIENCE', 'AKASH', 10000),
+('222-SCIENCE', 'ANDREW', 10000),
+('22-CHEM', 'ANKIT', 25000),
+('3333-CHEM', 'SONIKA', 12000),
+('4444-BIO', 'HITESH', 2300),
+('44-BIO', 'AKSHAY', 10000);
+-- Step 1: Create table and insert data
+CREATE TABLE dept_tbl (
+    id_deptname VARCHAR(50),
+    emp_name VARCHAR(50),
+    salary INT
+);
+
+-- Insert data
+INSERT INTO dept_tbl (id_deptname, emp_name, salary) VALUES
+('1111-MATH', 'RAHUL', 10000),
+('1111-MATH', 'RAKESH', 20000),
+('2222-SCIENCE', 'AKASH', 10000),
+('222-SCIENCE', 'ANDREW', 10000),
+('22-CHEM', 'ANKIT', 25000),
+('3333-CHEM', 'SONIKA', 12000),
+('4444-BIO', 'HITESH', 2300),
+('44-BIO', 'AKSHAY', 10000);
+
+--Here i am using  group function--
+
+select *
+from dept_tbl;
+select id_deptname, sum(salary) as Total_salary
+from dept_tbl
+group by id_deptname;
