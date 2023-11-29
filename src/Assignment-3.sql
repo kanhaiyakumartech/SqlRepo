ed4d8236dbeb703891743b1be25205edb2911aaf
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2023-11-29 10:08:37 +0530

    	 I Created a table product_details with above data

---
+++ src/Assignment-3.sql
@@ -0,0 +1,23 @@
+-- Step 1: Create table and insert data
+CREATE TABLE product_details (
+    sell_date DATE,
+    product VARCHAR(50)
+);
+
+-- Insert data
+INSERT INTO product_details  VALUES
+('2020-05-30', 'Headphones'),
+('2020-06-01', 'Pencil'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'Basketball'),
+('2020-06-01', 'Book'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'T-Shirt');
+
+select *
+from product_details;
+
+-- Step 2: Write a query to find the number of different products sold for each date
+SELECT sell_date,COUNT(DISTINCT product) AS num_sold,STRING_AGG(product, ', ') AS product_list
+FROM product_details
+GROUP BY sell_date;
